65f84e7e58b150d36fb322dc225c3426a8a29816
huy <ngogiahuy1503@gmail.com>
2015-12-10 11:33:45 +0700

    Version 2

--- doan/admin/index.php
+++ doan/admin/index.php
@@ -2,7 +2,19 @@
 include"connection.php";
 include"qtri.php";
  ?>
+ <?php 
+ ob_start();
+ session_start();
 
+ if(!isset($_SESSION['id']))
+ {
+	 header("location:../index.php");
+ }
+ else if($_SESSION['phanquyen']==0)
+ {
+	 header("location:../index.php");
+}
+ ?>
  
 <!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
 <html xmlns="http://www.w3.org/1999/xhtml">
